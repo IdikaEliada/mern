1563eff5b9f4c08e235cd038c9262e3453e028a2
Houtvie <asesprofuto@gmail.com>
2025-09-07 18:18:28 +0200

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import './index.css'
+import {Route, Routes} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NavBar from './components/NavBar'
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage /> } />
+        <Route path="/create" element={<CreatePage /> } />
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,47 +1,30 @@
-import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
-import { useColorMode } from '@chakra-ui/system';
 import { Link } from "react-router-dom";
-import { CgAddR } from "react-icons/cg";
-import { IoMoon } from "react-icons/io5";
-import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
-	const { colorMode, toggleColorMode } = useColorMode();
 
 	return (
-		<Container maxW={"1140px"} px={4}>
-			<Flex
-				h={16}
-				alignItems={"center"}
-				justifyContent={"space-between"}
-				flexDir={{
-					base: "column",
-					sm: "row",
-				}}
-			>
-				<Text
-					fontSize={{ base: "22", sm: "28" }}
-					fontWeight={"bold"}
-					textTransform={"uppercase"}
-					textAlign={"center"}
-					bgGradient={"linear(to-r, cyan.400, blue.500)"}
-					bgClip={"text"}
-				>
-					<Link to={"/"}>Product Store 🛒</Link>
-				</Text>
-
-				<HStack spacing={2} alignItems={"center"}>
-					<Link to={"/create"}>
-						<Button>
-							<CgAddR fontSize={20} />
-						</Button>
-					</Link>
-					<Button onClick={toggleColorMode}>
-						{colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
-					</Button>
-				</HStack>
-			</Flex>
-		</Container>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link class="flex items-center space-x-3 rtl:space-x-reverse" to="/">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Product Store</span>
+          <svg className="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M4 4a1 1 0 0 1 1-1h1.5a1 1 0 0 1 .979.796L7.939 6H19a1 1 0 0 1 .979 1.204l-1.25 6a1 1 0 0 1-.979.796H9.605l.208 1H17a3 3 0 1 1-2.83 2h-2.34a3 3 0 1 1-4.009-1.76L5.686 5H5a1 1 0 0 1-1-1Z" clip-rule="evenodd"/>
+          </svg>
+        </Link>
+        <div>
+          <Link to='/create' class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z" clip-rule="evenodd"/>
+            </svg>
+          </Link>
+          <button type='button' class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </nav>
 	);
 };
 export default Navbar;
